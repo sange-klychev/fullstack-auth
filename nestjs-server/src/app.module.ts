70a9e7fe003e9b00ce6ea42d5a76b264dc2425a9
Sandzhar <sanjargylyjov.dev@gmail.com>
2025-01-15 21:22:20 +0300

    finish configs

--- nestjs-server/src/app.module.ts
+++ nestjs-server/src/app.module.ts
@@ -1,6 +1,17 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+
+import {AuthModule} from './auth/auth.module';
+import {IS_DEV_ENV} from './libs/common/utils/is-dev.util';
+import {PrismaModule} from './prisma/prisma.module';
+import {UserModule} from './user/user.module';
 
 @Module({
-  imports: []
+	imports: [
+		ConfigModule.forRoot({ignoreEnvFile: !IS_DEV_ENV, isGlobal: true}),
+		PrismaModule,
+		AuthModule,
+		UserModule
+	]
 })
 export class AppModule {}
